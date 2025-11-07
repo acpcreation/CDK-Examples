45633b50a45f6c77bbdb7412bf493784389f773a
Alex Plattel <acpcreation@Alexs-MacBook-Pro.local>
2025-11-07 09:01:25 -0500

    create clouformation folder

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+
+
+# SET UP
+1. npm install -g aws-cdk 
+2. AWS CLI
+3. npm install -g typescript
+4. cdk init app --language typescript
+5. cdk bootstrap
+
+
+## Prep for SG
+cdk synth > cloudformation/cf-template.json
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,81 @@
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { version } from 'os';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Get the default VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', { 
+      isDefault: true 
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // Create a security group for the EC2 instance
+    const securityGroup = new ec2.SecurityGroup(this, 'Ec2SecurityGroup', {
+      vpc,
+      description: 'Security group for EC2 instance',
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH access
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH access'
+    );
+
+    // Allow HTTP access
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP access'
+    );
+
+    // Create the EC2 instance
+    const ec2Instance = new ec2.Instance(this, 'MyEc2Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux2(),
+      securityGroup,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      // keyName: 'your-key-pair-name', // Uncomment and add your key pair name for SSH access
+    });
+
+    // Add user data to install and start a web server
+    ec2Instance.addUserData(
+      'yum update -y',
+      'yum install -y httpd',
+      'systemctl start httpd',
+      'systemctl enable httpd',
+      'echo "<h1>Hello from CDK EC2 Instance!</h1>" > /var/www/html/index.html'
+    );
+
+    // Output the public IP address
+    new cdk.CfnOutput(this, 'InstancePublicIP', {
+      value: ec2Instance.instancePublicIp,
+      description: 'Public IP address of the EC2 instance',
+    });
+
+    // // L1 and L2 construct of S3 Bucket
+    // // L1
+    // const level1Bucket = new CfnBucket(this, 'myFirstLevel1ConstructBucket',{
+    //     versioningConfiguration: {
+    //         status: 'Enabled'
+    //     }
+    // });
+
+    // // L2
+    // const level2Bucket = new Bucket (this, 'myFirstLevel2ConstructBucket',{
+    //     bucketName: "s3-bucket-with-cdk-level-2-hsducybweuye2gwg62363676363",
+    //     versioned: true
     // });
   }
 }
